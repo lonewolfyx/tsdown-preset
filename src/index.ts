3397b02b57370c8085ad0b720cd15fef5a8bb6b3
lonewolfyx <877085893@qq.com>
2026-01-31 22:37:52 +0800

    feat(config): add vue, cli and lib presets for bundling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,37 @@
-export function test() {
-    console.log('hello, tsdown')
-    console.log(new Date())
+import type { UserConfig } from 'tsdown'
+import { mergeConfig } from 'tsdown'
+
+export const baseConfig = (overrides: UserConfig = {}): UserConfig => {
+    return mergeConfig({
+        exports: true,
+        dts: true,
+        sourcemap: true,
+    }, overrides)
+}
+
+export const vue = (overrides: UserConfig = {}): UserConfig => {
+    return baseConfig({
+        platform: 'neutral',
+        fromVite: true,
+        dts: {
+            vue: true,
+        },
+        ...overrides,
+    })
+}
+
+export const cli = (overrides: UserConfig = {}): UserConfig => {
+    return baseConfig({
+        entry: 'src/cli.ts',
+        platform: 'node',
+        ...overrides,
+    })
+}
+
+export const lib = (overrides: UserConfig = {}): UserConfig => {
+    return baseConfig({
+        entry: 'src/**/*.ts',
+        platform: 'neutral',
+        ...overrides,
+    })
 }
